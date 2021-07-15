f3ab7da8ee0694d2491a4e6fd61a6e2df4557b88
leenja <leenja@yandex.ru>
2021-07-15 15:23:03 +0300

    Первая версия проекта

---
+++ src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { FilmComponent } from './film/film.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FilmComponent   
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
