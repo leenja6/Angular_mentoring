9ece31f8935a393d3fc576c5c60bb8068c818d65
leenja <leenja@yandex.ru>
2021-07-29 12:50:50 +0300

    задание 3 v2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,33 +1,19 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
-import { FilmComponent } from './film/film.component';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
-import { CoursesComponent } from './courses/courses.component';
-import { SearchComponent } from './search/search.component';
-import { BtnAddCoursesComponent } from './btn-add-courses/btn-add-courses.component';
-import { LoreMoreComponent } from './lore-more/lore-more.component';
-import { BtnEditDeleteCoursesComponent } from './btn-edit-delete-courses/btn-edit-delete-courses.component';
-
+import { CommonsModule } from './commons/commons.module';
+import { CoreModule } from './core-module/core-module.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FilmComponent,
-    HeaderComponent,
-    FooterComponent,
-    CoursesComponent,
-    SearchComponent,
-    BtnAddCoursesComponent,
-    LoreMoreComponent,
-    BtnEditDeleteCoursesComponent   
-  ],
+   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    CommonsModule,
+    CoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
